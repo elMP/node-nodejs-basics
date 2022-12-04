784674fae9d5550aec12c4e01a981a12385c3739
kneazle <kneazle@yandex.ru>
2022-12-05 02:44:02 +0300

    feat: implement create file function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const myFile = path.join(path.dirname('.'), 'files', 'fresh.txt');
+  fs.exists(myFile, (ifFileExist) => {
+    if (ifFileExist) {
+      throw new Error('FS operation failed');
+    } else {
+      fs.writeFile(myFile, 'I am fresh and young', function (err) {
+        if (err) throw err;
+        console.log('File created');
+      });
+    }
+  });
 };
 
 await create();
