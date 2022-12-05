8e80d23fcce3b67ebac3806028553de6f6460788
kneazle <kneazle@yandex.ru>
2022-12-05 14:53:53 +0300

    feat: implement list folder function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import * as fs from 'fs/promises';
+import * as path from 'path';
+
 const list = async () => {
-    // Write your code here 
+  const pathToFilesFolder = path.join(path.dirname('.'), 'files');
+  try {
+    let files = await fs.readdir(pathToFilesFolder, { withFileTypes: true });
+    console.log("Files in directory 'files':");
+    files.forEach((file) => {
+      console.log(file.name);
+    });
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
