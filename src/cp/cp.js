764b6c44116e255eae0f413c30ad025efe48673c
kneazle <kneazle@yandex.ru>
2022-12-06 02:02:45 +0300

    feat implement child process subtask

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,18 @@
+import * as path from 'path';
+import { spawn } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childFile = path.join(path.dirname('.'), 'files', 'script.js');
+
+  const childProcess = spawn('node', [childFile, ...args]);
+
+  childProcess.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
+
+  process.stdin.on('data', (data) => {
+    childProcess.stdin.write(data);
+  });
 };
 
-spawnChildProcess();
+spawnChildProcess(['arg1', 'arg2', 'arg3']);
