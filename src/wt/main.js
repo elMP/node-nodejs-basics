1f83046c5d7a6c2ff5533598ce0fadd1eb884f03
kneazle <kneazle@yandex.ru>
2022-12-06 01:11:13 +0300

    feat implement workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,24 @@
+import { Worker } from 'worker_threads';
+import * as os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+  const osCount = os.cpus().length;
+  let workers = [];
+
+  for (let i = 10; i < 10 + osCount; i++) {
+    workers.push(
+      new Promise((resolve, reject) => {
+        const worker = new Worker('./worker.js', {
+          workerData: { n: i },
+        });
+        worker.on('message', (data) => resolve({ status: 'resolved', data }));
+        worker.on('error', () => reject({ status: 'error', data: null }));
+      })
+    );
+  }
+
+  const result = await Promise.all(workers);
+  console.log(result);
 };
 
 await performCalculations();
