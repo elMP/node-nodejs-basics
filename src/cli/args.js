3b947d6bc4070d39ed27947c4906f19586942cfb
kneazle <kneazle@yandex.ru>
2022-12-05 19:43:34 +0300

    feat: implement another function that parse environment variables

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  let output = '';
+
+  for (let i = 2; i < process.argv.length; i++) {
+    let arg = process.argv[i];
+    if (i % 2 === 0) {
+      output += arg.substring(2) + ' is ';
+    } else {
+      output += arg + ', ';
+    }
+  }
+  console.log(output.substring(0, output.length - 2));
 };
 
 parseArgs();
