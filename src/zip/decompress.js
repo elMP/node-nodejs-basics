6e8fc1cc8fdc089628a8e3bdf5bdaa8f27fb0aca
kneazle <kneazle@yandex.ru>
2022-12-05 17:22:18 +0300

    feat: implement arhive decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as zlib from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+  const myFile = path.join(path.dirname('.'), 'files', 'fileToCompress.txt');
+  const outputStream = fs.createWriteStream(myFile);
+
+  const archiveFile = path.join(path.dirname('.'), 'files', 'archive.gz');
+  const inputStream = fs.createReadStream(archiveFile);
+  const unzip = zlib.createUnzip();
+
+  inputStream.pipe(unzip).pipe(outputStream);
 };
 
 await decompress();
