60dab8dff7ebe47315330fb853e0fe68829672bb
kneazle <kneazle@yandex.ru>
2022-12-05 16:57:40 +0300

    feat: implement function that calculates hash for file content

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import * as crypto from 'crypto';
+import * as fs from 'fs/promises';
+import * as path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const pathToFileToRead = path.join(
+    path.dirname('.'),
+    'files',
+    'fileToCalculateHashFor.txt'
+  );
+
+  const content = await fs.readFile(pathToFileToRead, { encoding: 'utf8' });
+  const hash = crypto
+    .createHmac('sha256', 'I forgot it')
+    .update(content)
+    .digest('hex');
+  console.log(hash);
 };
 
 await calculateHash();
