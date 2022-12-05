2bc50ee44c8c3bd94f3892e66cf97a7f29186378
kneazle <kneazle@yandex.ru>
2022-12-05 17:52:24 +0300

    feat: implement function that parse environment variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  let output = '';
+  for (let i = 0; i < process.argv.length; i++) {
+    let arg = process.argv[i];
+    if (arg.substring(0, 4) === 'RSS_') {
+      output += arg + '; ';
+    }
+  }
+  console.log(output.substring(0, output.length - 2));
 };
 
 parseEnv();
