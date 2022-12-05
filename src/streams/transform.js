db772840f3bd8ff4036b9b4cfbc5d9e6f1240d64
kneazle <kneazle@yandex.ru>
2022-12-05 16:25:19 +0300

    feat: change input using Transform Stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform, pipeline } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  function reverseString(str) {
+    var arrFromString = str.split('');
+    var reverseArr = arrFromString.reverse();
+    var joinArr = reverseArr.join('');
+    return joinArr;
+  }
+
+  const reverseTransformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, reverseString(chunk.toString()) + '\n');
+    },
+  });
+
+  pipeline(process.stdin, reverseTransformStream, process.stdout, () => {});
 };
 
 await transform();
