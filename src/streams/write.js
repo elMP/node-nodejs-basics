1590c1a483bf60dfe8f1b8eae301366fd2bba850
kneazle <kneazle@yandex.ru>
2022-12-05 16:00:07 +0300

    feat: write to file using Writable Stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
 const write = async () => {
-    // Write your code here 
+  const myFile = path.join(path.dirname('.'), 'files', 'fileToWrite.txt');
+  const myStream = fs.createWriteStream(myFile);
+
+  console.log('Write something. To stop - press ctrl + c');
+  process.stdin.pipe(myStream);
+  process.stdin.resume();
 };
 
 await write();
